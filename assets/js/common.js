154154c61024cd2826bfd4040ea44559fbdd90b7
DESKTOP-HPKO9NR\hanmi <hanmi8080@gmail.com>
2021-07-27 21:46:33 +0900

    매칭방 만들기 plan A

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,23 @@
-$(function(){
-    $('.container').css('height',$(window).height() - $('.appbar').outerHeight(true) - $('.tabbar').outerHeight(true));
+// container 높이
+function base() {
+    $('a').click(function(e){
+        if($(e.target).attr('href') === '#' || $(e.target).parent().attr('href') === '#'){
+            e.preventDefault()
+        }
+    })
+    $('.container').css('height', $(window).height() - $('.appbar').outerHeight(true) - $('.tabbar').outerHeight(true));
+}
 
+
+// 검색창 접기
+function folding(){
     $('.folding').find('button').click(function(){
         $('.folding-contents').slideToggle(200);
         $(this).children('i').toggleClass('fa-chevron-down fa-chevron-up');
     })
-
-    //tabs
+}
+//tabs
+function tabs() {
     $('.tabs').find('a').click(function(){
         var tabID = $(this).attr('href');
         $(this).parent('li').siblings().removeClass('on')
@@ -14,18 +25,49 @@ $(function(){
         $(this).parents('.tabs').siblings('.tab-contents').removeClass('on')
         $(tabID).addClass('on');
     })
+}
+// reserve-tabs
+function reserveTabs() {
+    $('.reserve-tabs').find('a').click(function(){
+        var tabID = $(this).attr('data-target');
+        $(this).parent('li').siblings().removeClass('on')
+        $(this).parent('li').addClass('on');
+        $(this).parents('.reserve-tabs').siblings('.reserve-tab-contents').removeClass('on')
+        $(tabID).addClass('on');
+    })
+}
 
-    // modal open
+// modal open
+function modalCall() {
     $('.modal-call').click(function(){
         var modalID = $(this).attr('data-target');
         $(modalID).show();
     })
-    // modal close
+}
+// modal close
+function modalClose() {
     $('.modal-close').click(function(){
         $(this).parents('.modal').hide();
     })
+}
 
-    // button - toggle
+function listSelected () {
+    $('.list-selected').children('li').click(function(){
+        $(this).siblings('li').removeClass('on')
+        $(this).addClass('on')
+    })
+}
+
+function normalBtn() {
+    $('.normal-btn').children().click(function(){
+        $(this).siblings().removeClass('on')
+        $(this).addClass('on')
+    })
+}
+
+// button - toggle
+function buttonToggle() {
+
     $('.toggle-btn').find('button').click(function(){
         if($(this).hasClass('general')){
             $(this).removeClass('btn-default')
@@ -43,9 +85,48 @@ $(function(){
         $(this).parent().siblings('.tab-contents').removeClass('on')
         $(buttonID).addClass('on')
     })
-    // button - check
+}
+
+// button - check
+function buttonCheck() {
     $('.check-btn').find('button').click(function() {
         $(this).toggleClass('btn-default btn-success');
     })
+}
 
-})
+// 공지사항 (접기/펼치기)
+function bbs(){
+    $('.bbs_list').find('a').click(function(){
+        $(this).siblings('.bbs_contents').slideToggle(200);
+        $(this).children('i.slideToggle').toggleClass('fa-caret-down fa-caret-up')
+    })
+}
+function iChecks() {
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+    });
+}
+
+function reserveTime() {
+    $('.reserve-time').find('li').click(function(){
+        if(!$(this).hasClass('disabled')){
+            $(this).toggleClass('on')
+        }
+    })
+}
+function possible(){
+    $('.badge-wrap').find('em').click(function(){
+        $(this).toggleClass('on')
+    })
+}
+
+function offday() {
+    $('.day').find('li').click(function(){
+        $(this).toggleClass('on')
+    })
+}
+
+
+
+
